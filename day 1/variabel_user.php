28151ca2dad0e46ddceadbbe012a117bc433d68a
mirahgarnida11 <146153792+mirahgarnida11@users.noreply.github.com>
2024-03-02 18:44:29 +0700

    Add files via upload

---
+++ day 1/variabel_user.php
@@ -0,0 +1,10 @@
+<?php
+ // definisikan variables
+ $nama = 'Mirah Delima Garnida';
+ $umur = 20;
+ $berat = 48.0;
+ echo 'Nama : ' . $nama;
+ echo '<br/>Umur : ' . $umur.' Tahun';
+ echo '<br/>Berat : '.$berat.' Kg';
+ echo "<br/>Hello $nama Apakabar";
+ ?>
